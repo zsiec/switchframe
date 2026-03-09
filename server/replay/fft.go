5142a81b750ff2d68f8c961fb42aa048ec228805
Thomas Symborski <thomas.symborski@gmail.com>
2026-03-09 01:40:15 -0400

    feat(replay): add radix-2 butterfly and in-place C2C FFT

--- server/replay/fft.go
+++ server/replay/fft.go
@@ -48,3 +48,51 @@ func newFFT(n int) *fftState {
 		bitrev:  bitrev,
 	}
 }
+
+// forward performs an in-place forward FFT on interleaved complex data.
+// data has length 2*n (n complex values as [re0, im0, re1, im1, ...]).
+func (f *fftState) forward(data []float32) {
+	n := f.n
+
+	// Bit-reversal permutation
+	for i := 0; i < n; i++ {
+		j := f.bitrev[i]
+		if i < j {
+			// Swap complex values
+			data[2*i], data[2*j] = data[2*j], data[2*i]
+			data[2*i+1], data[2*j+1] = data[2*j+1], data[2*i+1]
+		}
+	}
+
+	// Iterative Cooley-Tukey: log2N stages
+	for s := 1; s <= f.log2N; s++ {
+		m := 1 << s       // number of elements in each group
+		halfM := m / 2     // number of butterflies per group
+		twStride := n / m  // twiddle stride for this stage
+
+		for groupStart := 0; groupStart < n; groupStart += m {
+			butterflyRadix2(data[groupStart*2:], f.twiddle, halfM, 1, twStride)
+		}
+	}
+}
+
+// inverse performs an in-place inverse FFT on interleaved complex data.
+// data has length 2*n. Result is scaled by 1/n.
+func (f *fftState) inverse(data []float32) {
+	n := f.n
+
+	// Conjugate twiddle factors (negate imaginary parts in data)
+	for i := 0; i < n; i++ {
+		data[2*i+1] = -data[2*i+1]
+	}
+
+	// Forward FFT
+	f.forward(data)
+
+	// Conjugate again and scale by 1/N
+	scale := float32(1.0) / float32(n)
+	for i := 0; i < n; i++ {
+		data[2*i] *= scale
+		data[2*i+1] *= -scale
+	}
+}
